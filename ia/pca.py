e27488d77f34c674792c2dedb9e28e66c9fef65f
Guitheg <34913527+Guitheg@users.noreply.github.com>
2021-12-14 09:06:24 +0100

    Update pca.py

--- ia/pca.py
+++ ia/pca.py
@@ -1,8 +1,11 @@
 import os
+from re import T
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import plotly
+import plotly.express as px
+import plotly.graph_objects as go
 from pathlib import Path
 from sklearn.decomposition import PCA
 from freqtrade.data.history.history_utils import load_pair_history
@@ -12,21 +15,40 @@ from sklearn.preprocessing import StandardScaler
 def process_pca(data_df: pd.DataFrame, n_components: int, plot: bool = False):
     data = np.array(data_df)[:,1:]  # to erase timestamp
     data = StandardScaler().fit_transform(data)
+
     pca = PCA(n_components=n_components)
     pca_data_result = pca.fit_transform(data)
     pcd_data_df_result = pd.DataFrame(data=pca_data_result, 
-                                      columns=[f"C{i}" for i in range(n_components)])
+                                      columns=[f"C{i+1}" for i in range(n_components)])
+    pcd_data_df_result["date"] = data_df["date"]
+    pcd_data_df_result["color"] = np.linspace(0, 255, len(pcd_data_df_result))
     if plot == True:
         if n_components == 2:
-            fig = plt.figure(figsize=(8,8))
-            ax = fig.add_subplot(1,1,1)
-            ax.set_xlabel(pcd_data_df_result.columns[0])
-            ax.set_ylabel(pcd_data_df_result.columns[1])
-            ax.set_title('2 component PCA', fontsize=20)
-            ax.scatter(pcd_data_df_result[pcd_data_df_result.columns[0]], 
-                       pcd_data_df_result[pcd_data_df_result.columns[1]])
-            ax.grid()
-            plt.show()
+            fig = plotly.subplots.make_subplots(rows=2, cols=1)
+            trace = px.scatter(pcd_data_df_result,
+                                         text = 'date',
+                                         x = pcd_data_df_result.columns[0],
+                                         y = pcd_data_df_result.columns[1],
+                                         color = 'color')["data"]
+            # trace[0]["mode"] = 'lines'
+            trace[0]["mode"] = 'markers'
+            trace[0]["marker"]["symbol"] = "circle"
+            trace[0]["marker"]["size"] = 3
+            fig.add_trace(
+                trace[0],
+                row=1,
+                col=1
+            )
+            data_df["color"] = pcd_data_df_result["color"]
+
+            theline = px.scatter(data_df, x="date", y="close", color="color")["data"]
+            theline[0]["mode"] = "markers"
+            fig.add_trace(
+                theline[0],
+                row=2,
+                col=1
+            )
+            fig.show()
         else:
             raise NotImplementedError("n_component =/= 2 not implemented")
     
@@ -34,7 +56,7 @@ def process_pca(data_df: pd.DataFrame, n_components: int, plot: bool = False):
 
 def main():
     print(os.path.abspath(os.path.curdir))
-    BTC_BUSD_5m = np.array(load_pair_history("BTC/BUSD", "5m", Path("./user_data/data/binance")))[:,1:]
+    BTC_BUSD_5m = load_pair_history("BTC/BUSD", "1m", Path("./user_data/data/binance"))
     pca_result = process_pca(BTC_BUSD_5m, n_components=2, plot=True)
     
 if __name__ == "__main__":
