f88331927e0c0cd9c38174d945e3537b4346333f
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-01-05 09:51:17 +0100

    Update pca.py

--- ia/pca.py
+++ ia/pca.py
@@ -3,10 +3,8 @@ from re import T
 from typing import Callable, List
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 import plotly
 import plotly.express as px
-import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from pathlib import Path
 from sklearn.decomposition import PCA
@@ -14,86 +12,13 @@ from freqtrade.data.history.history_utils import load_pair_history
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.cluster import KMeans
-
-
-COL = ["open", "high", "low", "close", "volume"]
-
+import argparse
 
 def dataframe_to_numpy(data_df: pd.DataFrame,
                        columns: List[str]) -> np.ndarray:
     data = np.array(data_df[columns])
         
     return data
-    
-def process_pca(data_df: pd.DataFrame, n_components: int, plot: bool = False) -> pd.DataFrame:
-    data = dataframe_to_numpy(data_df, columns=COL)
-    data = standardize_data(data)
-
-    pca = PCA(n_components=n_components)
-    pca_data_result = pca.fit_transform(data)
-    pcd_data_df_result = pd.DataFrame(data=pca_data_result, 
-                                      columns=[f"C{i+1}" for i in range(n_components)])
-    pcd_data_df_result["date"] = data_df["date"]
-    pcd_data_df_result["color"] = np.linspace(0, 255, len(pcd_data_df_result))
-    
-    if n_components == 2:
-        fig = plotly.subplots.make_subplots(rows=3, cols=2)
-        trace = px.scatter(pcd_data_df_result,
-                                        text = 'date',
-                                        x = pcd_data_df_result.columns[0],
-                                        y = pcd_data_df_result.columns[1],
-                                        color = 'color')["data"]
-        # trace[0]["mode"] = 'lines'
-        trace[0]["mode"] = 'markers'
-        trace[0]["marker"]["symbol"] = "circle"
-        trace[0]["marker"]["size"] = 3
-        fig.add_trace(
-            trace[0],
-            row=1,
-            col=1
-        )
-        
-        data_df["color"] = pcd_data_df_result["color"]
-        theline = px.scatter(data_df, x="date", y="close", color="color")["data"]
-        theline[0]["mode"] = "markers"
-        fig.add_trace(
-            theline[0],
-            row=2,
-            col=1
-        )
-        print(pca.noise_variance_)
-        print(pca.get_covariance())
-        df_components = pd.DataFrame(data=pca.components_.T, columns=['pc1', 'pc2'])
-        print(df_components)
-
-        compo = px.scatter(df_components, x="pc1", y="pc2")["data"][0]
-        fig.add_trace(
-            compo,
-            row=3,
-            col=2
-        )
-        
-        hist = px.histogram(y=pca.explained_variance_ratio_, x=np.arange(len(pca.explained_variance_ratio_)))["data"][0]
-        fig.add_trace(
-            hist,
-            row=1,
-            col=2
-        )
-        
-        hist = px.histogram(y=pca.singular_values_, x=np.arange(len(pca.explained_variance_ratio_)))["data"][0]
-        fig.add_trace(
-            hist,
-            row=2,
-            col=2
-        )
-        
-        if plot == True:
-            fig.show()
-    else:
-        raise NotImplementedError("n_component =/= 2 not implemented")
-
-    return pcd_data_df_result, (trace[0], theline[0])
-
 
 def _to_pd(fct: Callable, 
            data_df: pd.DataFrame, 
@@ -153,29 +78,11 @@ def data_trend_one_axis(data: np.ndarray, num_axis: int = 3) -> np.ndarray:
     return data_trend, (linear_regression.coef_, linear_regression.intercept_)
 
 
-def binary_soft_search(x, list: List, low = 0, high = -1):
-    high = len(list)-1 if high == -1 else high
-    if high <= low:
-        if low <= 0 :
-            return 0
-        else:
-            return len(list)-1
-    mid = int((low + high) / 2)
-    if x == list[mid] or x > list[mid] and x < list[mid+1]:
-        return mid
-    if x < list[mid]:
-        return binary_soft_search(x, list, low, mid-1)
-    if x > list[mid]:
-        return binary_soft_search(x, list, mid+1, high)
-
-
-
 def analyse_acp(data_df: pd.DataFrame, 
                 columns : List[str], 
                 mode : str = "default",
                 input_plot : str = None,
                 name : str = None,
-                do_kmeans : bool = True,
                 n_cluster : int = 2,
                 n_cpt_kmeans : int = 2,
                 plot : bool = True) -> dict:
@@ -198,11 +105,11 @@ def analyse_acp(data_df: pd.DataFrame,
     acp_results = acp.fit_transform(data[columns])
     acp_results_df = pd.DataFrame(data=acp_results, 
                                     columns=[f"F{i+1}" for i in range(acp.n_components_)])
-    if do_kmeans:
-        k_means = KMeans(n_clusters=n_cluster)
-        k_means.fit(acp_results_df[[f"F{i+1}" for i in range(n_cpt_kmeans)]])
-        acp_results_df['label'] = k_means.labels_
-        data['label'] = k_means.labels_
+
+    k_means = KMeans(n_clusters=n_cluster)
+    k_means.fit(acp_results_df[[f"F{i+1}" for i in range(n_cpt_kmeans)]])
+    acp_results_df['label'] = k_means.labels_
+    data['label'] = k_means.labels_
 
     explained_variance_ratio = acp.explained_variance_ratio_
     cumul_explained_variance_ratio = np.cumsum(explained_variance_ratio)
@@ -224,7 +131,7 @@ def analyse_acp(data_df: pd.DataFrame,
 
     # 1 ###
     data_trace_df = data.copy()
-    data_trace_df["color"] = data['label'] if do_kmeans else np.linspace(0, 255, len(acp_results_df))
+    data_trace_df["color"] = data['label']
 
     input_data = px.scatter(data_trace_df, x="date", y=input_plot, 
                             color='color', 
@@ -340,6 +247,7 @@ def analyse_acp(data_df: pd.DataFrame,
     
     result = {}
     result["data"] = data
+    result["kmeans"]
     result["fig"] = fig
     result["acp"] = acp
     result["coord"] = acp_results_df
@@ -351,77 +259,43 @@ def analyse_acp(data_df: pd.DataFrame,
     
 
 def main():
+    parser = argparse.ArgumentParser()
+    
+    ### A propos des données
+    parser.add_argument("--pair_name", type=str, help="exemple : BTC/BUSD")
+    parser.add_argument("-t", "--timeframe", type=str, help="exemple : 1m ")
+    parser.add_argument("--path", type=str, help="chemin vers les données", 
+                        default="./user_data/data/binance")
+    
+    ### A propos de l'ACP et de l'analyse
+    parser.add_argument("--col", type=str, nargs='+', help="colonnes qui seront prise en compte ")
+    parser.add_argument("--input_ploted", type=str, 
+                        help="la colonne qui sera affiché en tant qu'entré")
+    parser.add_argument("--n_cluster", type=str, help="pour les kmeans")
+    parser.add_argument("--n_cpt_kmeans", type=str, 
+                        help="le nombre de facteur que les kmeans prendront en compte")
+    
+    args = parser.parse_args()
+    
     print(os.path.abspath(os.path.curdir))
-    BTC_BUSD_1m = load_pair_history("BTC/BUSD", "1m", Path("./user_data/data/binance"))
-    # print(BTC_BUSD_1m)
+    try:
+        BTC_BUSD_1m = load_pair_history(args.pair_name, 
+                                        args.timeframe, 
+                                        Path(args.path))
+    except:
+        Exception("Soit le chemin d'accès est éronné,"
+                  +" soit la pair en question n'a pas été téléchargé")
     
-    result_analyse = analyse_acp(BTC_BUSD_1m, COL,
-                name = "Cours du BTC", 
+    result_analyse = analyse_acp(BTC_BUSD_1m, args.col,
+                name = f"{args.pair_name}_{args.timeframe}", 
                 mode = "stationarize", 
-                input_plot="close",
-                n_cluster=3,
-                n_cpt_kmeans=3,
+                input_plot=args.input_ploted,
+                n_cluster=args.n_cluster,
+                n_cpt_kmeans=args.n_cpt_kmeans,
                 plot=True)
-    
-    data_km = result_analyse["data"]
-    coord_acp = result_analyse["coord"]
-    kmeans = KMeans(n_clusters=4)
-    # k = kmeans.fit(data_km[COL])
-    k = kmeans.fit(coord_acp[["F1", "F2"]])
 
-    data_km["label"] = k.labels_
-    coord_acp["label"] = k.labels_
-    print(data_km)
-    
-    fig = px.scatter(data_km, x="date", y="close", color="label")
-    fig.show()
-    fig = px.scatter(coord_acp, x="F1", y="F2", color="label")
-    fig.show()
-    # data = dataframe_to_numpy(BTC_BUSD_1m, columns=COL)
-    
-    # data = standardize_data(data)
-    # data_df = standardize_data_df(BTC_BUSD_1m, columns=COL)
-
-    # untrended_data = untrend_data(data)
-    # untrended_data_df = untrend_data_df(data_df, columns=COL)
-    
-    # stationarized_data = stationnarize_data(data)
-    # stationarized_data_df = stationnarize_data_df(data_df, columns=COL)
-    
-    # data_trend,_ = data_trend_one_axis(data, 3)
-
-    # acp = PCA()
-    # r = acp.fit_transform(stationarized_data_df[COL])
-    # hist = px.bar(y=acp.explained_variance_ratio_, x=np.arange(len(acp.explained_variance_ratio_)), title="explained variance ratio")
-    # hist.show()
-    # hist = px.bar(y=acp.singular_values_, x=np.arange(len(acp.singular_values_)), title="singular_values")
-    # hist.show()
-    # pca_result_untrended, (pca1, line1) = process_pca(untrended_data_df, n_components=2, plot=True)
-    # pca_result_stationarized, (pca2, line2) = process_pca(stationarized_data_data_df, n_components=2, plot=True)
-
-
-
-
-    # histogram
-    # hist_varia = px.histogram(stationarized_data_data_df, x="close", title="ratio variation histogram")
-    # hist_cov = px.histogram()
-    # print(hist_varia)
-    
-    # fig = plotly.subplots.make_subplots(rows=4, cols=2)
-    # fig.add_trace(hist_varia['data'][0],  row=4, col=1)
-    # fig.add_trace(go.Scatter(x=np.arange(len(data)), y=data[:,3]), row=1, col=1)
-    # fig.add_trace(go.Scatter(x=np.arange(len(data)), y=data_trend, mode="lines"), row=1, col=1)
-    # fig.add_trace(line2, row=2, col=1)
-    # fig.add_trace(line1, row=3, col=1)
-    
-    # fig.add_trace(pca1,  row=3, col=2)
-    # fig.add_trace(pca2,  row=2, col=2)
-    # fig.show()
-    
-    # process_pca(untrended_data_df, n_components=2, plot=True)
-    # process_pca(stationarized_data_df, n_components=2, plot=True)
-
-    pass
-    
+    for name in result_analyse:
+        print(result_analyse[name])
+        
 if __name__ == "__main__":
     main()
