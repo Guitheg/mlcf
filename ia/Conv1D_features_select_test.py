c1521d796d8e55968a7c3d1242bbea05eaa8693d
EmbraysiteReal <46327982+EmbraysiteReal@users.noreply.github.com>
2022-01-31 15:52:16 +0100

    FIX

--- ia/Conv1D_features_select_test.py
+++ ia/Conv1D_features_select_test.py
@@ -16,28 +16,12 @@ learning_rate = 0.001
 
 # dataset has PILImage images of range [0, 1]. 
 # We transform them to Tensors of normalized range [-1, 1]
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
-                                          shuffle=True)
-
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
-                                         shuffle=False)
-print(test_loader.shape)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
+def data_to_tensor()
 
 # get some random training images
 dataiter = iter(train_loader)
--- main_ambroise.py
+++
@@ -1,8 +0,0 @@
-from ia import Conv1D_features_select_test, GA_feature_selection
-
-
-def main():
-    print("Hello World")    
-
-if __name__ == "__main__":
-    main()
