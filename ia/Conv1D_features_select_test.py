0a0dd919c5b022748e8457e7c54634bb51923964
EmbraysiteReal <46327982+EmbraysiteReal@users.noreply.github.com>
2022-02-03 14:06:14 +0100

    delete copy of datadools

--- ia/Conv1D_features_select_test.py
+++ ia/Conv1D_features_select_test.py
@@ -7,38 +7,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Hyper-parameters 
-num_epochs = 5
-batch_size = 4
-learning_rate = 0.001
-
-# dataset has PILImage images of range [0, 1]. 
-# We transform them to Tensors of normalized range [-1, 1]
-
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-def data_to_tensor()
-
-# get some random training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
 
 class ConvNet(nn.Module):
     def __init__(self, nb_channel):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv1d(nb_channel, nb_channel*2, 10)
+        k1,k2 = 3, 2
+        self.conv1 = nn.Conv1d(nb_channel, nb_channel*2, k1)
         # self.pool = nn.MaxPool1d(2)
-        self.conv2 = nn.Conv1d(nb_channel*2, nb_channel*4, 8)
+        self.conv2 = nn.Conv1d(nb_channel*2, nb_channel*4, k2)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 1)
 
     def forward(self, x):
         # -> n, 3, 32, 32
+        print(x.shape)
+        print(x)
         x = F.relu(self.conv1(x))  
         print(x.shape)
         x = F.relu(self.conv2(x)) 
@@ -51,60 +36,3 @@ class ConvNet(nn.Module):
         print(x.shape)
         x = self.fc3(x)                       
         return x
-
-model = ConvNet().to(device)
-
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-n_total_steps = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        # origin shape: [4, 3, 32, 32] = 4, 3, 1024
-        # input_layer: 3 input channels, 6 output channels, 5 kernel size
-        images = images.to(device)
-        labels = labels.to(device)
-
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if (i+1) % 2000 == 0:
-            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
-
-print('Finished Training')
-PATH = './cnn.pth'
-torch.save(model.state_dict(), PATH)
-
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    n_class_correct = [0 for i in range(10)]
-    n_class_samples = [0 for i in range(10)]
-    for test, labels in test_loader:
-        test = test.to(device)
-        labels = labels.to(device)
-        outputs = model(test)
-        # max returns (value ,index)
-        _, predicted = torch.max(outputs, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
-        
-        for i in range(batch_size):
-            label = labels[i]
-            pred = predicted[i]
-            if (label == pred):
-                n_class_correct[label] += 1
-            n_class_samples[label] += 1
-
-    acc = 100.0 * n_correct / n_samples
-    print(f'Accuracy of the network: {acc} %')
-
-    for i in range(10):
-        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-        print(f'Accuracy of {classes[i]}: {acc} %')
