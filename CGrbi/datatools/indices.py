32ab4bef86777224c87690d00323e28ef09623de
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-09 15:08:34 +0100

    Update indices.py

--- CGrbi/datatools/indices.py
+++ CGrbi/datatools/indices.py
@@ -1,4 +1,19 @@
+from typing import List
+from functools import partial
 import pandas as pd
-LIST_INDICATORS = []
 
-def add_indicator(dataframe : pd.DataFrame)
+# def add_indicator(indice_name : str, dataframe : pd.DataFrame):
+#     data = dataframe.copy()
+#     match indice_name:
+#         case "ADX": dataframe['adx'] = ta.ADX(dataframe)
+
+
+def add_indicators(dataframe : pd.DataFrame, list_indices : List[str], dropna : bool = True):
+    data = dataframe.copy()
+    
+    # for indice in list_indices:
+    #     data = add_indicator(indice, data)
+    
+    data.dropna(inplace=dropna)
+    return data
+
