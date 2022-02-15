bc44ef73a3c664dbf928083de1bd4aa97c87f8b3
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-15 15:39:33 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,14 +2,4 @@ from setuptools import setup
 
 
 if __name__ == "__main__":
-    setup(
-        tests_require=["pytest", 
-                    "pytest-mock"],
-        install_requires=["torch",
-                        "torchvision",
-                        "torchaudio",
-                        "tensorboard",
-                        "torch-tb-profiler",
-                        "freqtrade"],
-        setup_requires=['flake8']
-    )
+    setup()
