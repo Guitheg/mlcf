869f4037b4e4ec6b65ac292ff8975e624cd7728e
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-15 14:46:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,5 @@ import mlcf
 setup(
     tests_require=["pytest", 
                    "pytest-mock"],
-    install_requires=["freqtrade",
-                      "torch==1.10.2+cu113",
-                      "tensorboard",
-                      "torch-tb-profiler"],
     setup_requires=['flake8']
 )
