6b5514b87f5d82c1d392a5628ad7ede6d61e1083
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-16 10:53:58 +0100

    install_requires in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         dependency_links = [
-            "build_helper/TA_Lib-0.4.24-cp39-cp39-win_amd64.whl",
-            "https://download.pytorch.org/whl/cu113/torch-1.10.2%2Bcu113-cp39-cp39-linux_x86_64.whl"
+            "build_helper/TA_Lib-0.4.24-cp39-cp39-win_amd64.whl"
+        ],
+        install_requires = [
+            "ta-lib @ build_helper/TA_Lib-0.4.24-cp39-cp39-win_amd64.whl"
         ]
     )
