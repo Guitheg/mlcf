50319427de86818d13cf798c0c53982085d46bab
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-16 14:43:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class TalibDevelop(develop):
     def run(self) -> None:
         talib_install()
         return super().run()
-    
+
     def finalize_options(self) -> None:
         return super().finalize_options()
 
@@ -32,25 +32,13 @@ class TalibInstall(install):
     def run(self):
         talib_install()
         install.run(self)
-        
+
     def finalize_options(self) -> None:
         return super().finalize_options()
 
 
 talib_install()
 
-
-requirement_torch_linux = [
-    "torch @ https://download.pytorch.org/whl/cu113/torch-1.10.2%2Bcu113-cp39-cp39-linux_x86_64.whl",
-    "torchvision @ https://download.pytorch.org/whl/cu113/torchvision-0.11.3%2Bcu113-cp39-cp39-linux_x86_64.whl",
-    "torchaudio @ https://download.pytorch.org/whl/cu113/torchaudio-0.10.2%2Bcu113-cp39-cp39-linux_x86_64.whl"
-]
-requirement_torch_windows = [
-    "torch @ https://download.pytorch.org/whl/cu113/torch-1.10.2%2Bcu113-cp39-cp39-win_amd64.whl",
-    "torchvision @ https://download.pytorch.org/whl/cu113/torchvision-0.11.3%2Bcu113-cp39-cp39-win_amd64.whl",
-    "torchaudio @ https://download.pytorch.org/whl/cu113/torchaudio-0.10.2%2Bcu113-cp39-cp39-win_amd64.whl"
-]
-requirement_torch = requirement_torch_linux if CURRENT_OS == LINUX_OS else requirement_torch_windows
 install_requirements = [
     "setuptools==59.5.0",
     "tensorboard",
@@ -59,8 +47,6 @@ install_requirements = [
     "ccxt>=1.72.98",
     "freqtrade"
 ]
-install_requirements += requirement_torch
-
 
 setup(
     install_requires=install_requirements,
