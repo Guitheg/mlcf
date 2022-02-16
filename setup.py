aa7171fa52f044080144319100254d0f84c1c926
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-16 11:01:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,5 @@ if __name__ == "__main__":
     setup(
         dependency_links = [
             "build_helper/talib.whl"
-        ],
-        install_requires = [
-            "build_helper/talib.whl"
         ]
     )
