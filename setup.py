a1813412acb13829d485230cff319165245b83c5
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-16 11:00:04 +0100

    attempt

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         dependency_links = [
-            "build_helper/TA_Lib-0.4.24-cp39-cp39-win_amd64.whl"
+            "build_helper/talib.whl"
         ],
         install_requires = [
-            "build_helper/TA_Lib-0.4.24-cp39-cp39-win_amd64.whl"
+            "build_helper/talib.whl"
         ]
     )
