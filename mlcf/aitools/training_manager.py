cd4beee0f7a5626a0373137cffa33b8271d2a318
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-17 21:45:13 +0100

    Update training_manager.py

--- mlcf/aitools/training_manager.py
+++ mlcf/aitools/training_manager.py
@@ -93,11 +93,9 @@ class TrainingManager(object):
             self.home_path = self.project.get_dir()
             self.training_home = join(self.home_path, HOME)
             self.infofile_path = join(self.training_home, INFOFILE)
-            self.logs_path = create_path(str(self.training_home), LOGS)
-            self.models_path = create_path(str(self.training_home), MODELS)
-            self.checkpoint_path = create_path(str(self.models_path), CHECKPOINT,
+            self.checkpoint_path = create_path(str(self.training_home), CHECKPOINT,
                                                f"checkpoints_{self.model.training_name}")
-            self.board_path = create_path(str(self.logs_path), TENSORBOARD_LOGS_NAME,
+            self.board_path = create_path(str(self.training_home), TENSORBOARD_LOGS_NAME,
                                           f"boards_{self.model.training_name}")
             now = datetime.now()
             self.now = str(now.strftime("%Y-%d%b%Hh%Mm%S"))
