2ad21e4599c115c94ab90664e7fd5a9f6e04ceb8
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-02-19 14:33:25 +0100

    add pairs and timeframes arguments to choose selected rawdata

--- mlcf/datatools/datasetools.py
+++ mlcf/datatools/datasetools.py
@@ -1,5 +1,6 @@
+from functools import partial
 from pathlib import Path
-from typing import Generator, List
+from typing import Callable, Generator, List
 import pandas as pd
 
 from mlcf.datatools.indice import Indice, add_indicators
@@ -27,19 +28,26 @@ def read_ohlcv_json_rawdata(path: Path) -> pd.DataFrame:
     return data
 
 
-def read_all_ohlcv_rawdata_from_dir(dir: Path) -> Generator:
+def read_all_ohlcv_rawdata_from_dir(dir: Path, predicate_select: Callable) -> Generator:
     if not dir.is_dir():
         raise Exception(f"The path {dir} lead to any directory")
     for file in dir.iterdir():
-        if file.is_file() and file.suffix == ".json":
+        if file.is_file() and file.suffix == ".json" and predicate_select(file):
             data = read_ohlcv_json_rawdata(file)
             yield data
+
+
+def select_raw_data_based_on_name(pairs, timeframes, file):
+    pair, tf = file.stem.split("-")
+    return pair in [pair.replace("/", "_") for pair in pairs] and tf in timeframes
 
 
 def write_wtstdataset_from_raw_data(
     project: MlcfHome,
     rawdata_dir: Path,
     dataset_name: str,
+    pairs: List[str],
+    timeframes: List[str],
     input_size: int,
     target_size: int,
     offset: int,
@@ -50,6 +58,7 @@ def write_wtstdataset_from_raw_data(
     prop_v: float,
     indices: List[Indice],
     preprocess: WTSeriesPreProcess,
+    merge_pairs: bool,
     *args, **kwargs
 ):
 
@@ -60,10 +69,15 @@ def write_wtstdataset_from_raw_data(
         project=project
     )
 
-    for raw_data in read_all_ohlcv_rawdata_from_dir(rawdata_dir):
+    selected_rawdata = read_all_ohlcv_rawdata_from_dir(
+        rawdata_dir,
+        partial(select_raw_data_based_on_name, pairs, timeframes)
+    )
+    for raw_data in selected_rawdata:
         if indices:
             project.log.info(f"Adding indicators: {[i.value for i in indices]}")
             raw_data = add_indicators(raw_data, indices)
+
         dataset.add_time_serie(
             raw_data,
             prop_tv=prop_tv,
