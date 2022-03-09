600aafc0811746296c27766cabf0d4196332ca36
Guitheg <34913527+Guitheg@users.noreply.github.com>
2022-03-09 02:06:58 +0100

    refactoring indice

--- tests/datatools/test_indice.py
+++ tests/datatools/test_indice.py
@@ -9,7 +9,7 @@ def test_add_indicator(btc_ohlcv):
 
 def test_add_indicators(btc_ohlcv):
     assert len(btc_ohlcv.columns) == 6
-    list_indice = [Indice.ADX, Indice.P_DIDM]
+    list_indice = [Indice.ADX, Indice.P_DIM]
     data = add_indicators(btc_ohlcv, list_indice)
     assert len(data.columns) == 9
 
@@ -42,3 +42,10 @@ def test_stats(btc15m_ohlcv):
     data.dropna(inplace=True)
     assert len(data) == len(data[~data.isin([np.inf, -np.inf]).any(1)])
     assert len(data) != 0
+
+
+def test_indicators_list_to_std(btc_ohlcv):
+    list_indice = list(Indice)
+    list_to_std = set()
+    _ = add_indicators(btc_ohlcv, list_indice, standardize=True, list_to_std=list_to_std)
+    assert len(list_to_std) == 126
