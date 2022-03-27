c843a2d74a1cf914c18386678da23070734c2011
Guitheg <gthgobin@gmail.com>
2022-03-27 15:45:30 +0200

    Update utils.py
    documentation

--- mlcf/datatools/utils.py
+++ mlcf/datatools/utils.py
@@ -1,10 +1,14 @@
-"""Utilitaries module
-Provide some tools to perform processing on data frame"""
+"""Utilitaries module.
+
+Provide some tools to perform processing on data frame.
+"""
 
 from copy import copy
 from typing import List, Tuple, Union
 import pandas as pd
 import numpy as np
+
+# TODO (doc) review
 
 __all__ = [
     "binarize",
@@ -23,18 +27,17 @@ def binarize(
     label_col_name: str = None
 ) -> pd.DataFrame:
     """
-    Add a {label_col_name} column to a dataframe containing the binary label of a given column.
-    We can choose the separator value such as : every value of the column less than the {sep_value}
-    belong to the first class and every value greater or equal than the {sep_value} belong to the
-    second class. The greater or equal condition can be change by just a greater condition
-    if we put {include_sep} to False.
-    The new column will have the name of {label_col_name}, by default it will be :
-    '{column}_label'.
+    Add a {label_col_name} column to a dataframe containing the binary label of a feature.
+    We can choose the value of the separator as follows: any value in the column less than the
+    {sep_value} belongs to the first class and any value greater than or equal to the {sep_value}
+    belongs to the second class. The upper or equal condition can be changed to a simple upper
+    condition if we set {include_sep} to False.
+    The new column will have the name {label_col_name}, by default it will be: '{column}_label'.
 
     Args:
         data (pd.DataFrame): The dataframe
 
-        column (str): The column to binarize
+        column (str): The column name of the feature.
 
         labels (Union[Tuple, List], optional): A tuple or list of two element (the labels names).
             If None then the two elements will be (0, 1). Defaults to None.
@@ -82,11 +85,21 @@ def labelize(
     *args, **kwargs
 ) -> pd.DataFrame:
     """
-    Add a {label_col_name} column to a dataframe containing the labels of a given column.
+    Add a {label_col_name} column to a dataframe containing the labels of a given feature.
     The labels refer to a membership of a values to an interval. The number of intervals is
-    determined by the number of labels. All interval have the same size. If we give a bounds,
-    the intervals are determined inside the bounds. Every value which is under or above the bounds
-    will have respectively the label '-inf' and '+inf'.
+    determined by the number of labels. All interval have the same size.
+    Example:
+        Having the features X defined in the interval -1 and 1,
+        if we decide to label this features in 4 categories:
+        - Values between -1 and -0.5 belong to label 0,
+        - The values between -0.5 and 0 belong to the label 1,
+        - Values between 0 and 0.5 belong to label 2,
+        - Values between 0.5 and 1 belong to label 3.
+        0, 1, 2, 3 are labeled features of X.
+
+    If we give a bounds, the intervals are determined inside the bounds.
+    Every value which is under or above the bounds will have respectively the label
+    '-inf' and '+inf'.
 
     Args:
         data (pd.DataFrame): The dataframe
@@ -103,6 +116,27 @@ def labelize(
         label_col_name (str, optional): The column name of the new label column. By default, the
             name is : '{column}_label'. Defaults to None.
 
+    Example:
+
+    .. code-block:: python
+
+        from mlcf.datatools.utils import labelize
+
+        # A good practice is to take the mean and the standard deviation of the value you want to
+        # labelize
+        mean = data["return"].mean()
+        std = data["return"].std()
+
+        # Here you give the value you want to labelize with column='return'. The new of the labels
+        # column will be the name give to 'label_col_name'
+        data = labelize(
+            data,
+            column="return",
+            labels=5,
+            bounds=(mean-std, mean+std),
+            label_col_name="label"
+        )
+
     Raises:
         TypeError: Raise this exception if {bounds} is not a tuple of two elements.
 
@@ -111,7 +145,7 @@ def labelize(
         ValueError: If the value of label doesn't have a sense.
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: The data frame with the labeled feature added.
     """
     if data.isnull().values.any():
         raise Exception("NaN values has been found")
